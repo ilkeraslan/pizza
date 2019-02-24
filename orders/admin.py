2472679eb424f14f40b3ca0986baaeec0b242271
Dement0 <ilkeer.ozgur@gmail.com>
2019-02-24 19:17:22 +0100

    Add pizza to admin app.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pizza
+
+
+class PizzaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Pizza information', {'fields': ['pizza_name', 'pizza_type', 'pizza_description']}),
+        ('Pizza price', {'fields': ['pizza_price']})
+    ]
+
+    # Use list_display, a tuple of field names to display as columns
+    list_display = ('pizza_name', 'pizza_type', 'pizza_description')
+
+    # Use list filter to add a sidebar to filter
+    list_filter = ['pizza_type']
+
+    # Add a search box to search question texts, uses LIKE query
+    search_fields = ['pizza_name', 'pizza_description']
+
+
+admin.site.register(Pizza, PizzaAdmin)
--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,7 @@ class Pizza(models.Model):
     pizza_name = models.CharField(max_length=25)
     pizza_type = models.CharField(max_length=25)
     pizza_description = models.CharField(max_length=200)
-    pizza_price = models.IntegerField(default=0)
+    pizza_price = models.DecimalField(max_digits=4, decimal_places=2)
 
     def __str__(self):
         return f"{self.pizza_name} ({self.pizza_type}) -- {self.pizza_description}"
