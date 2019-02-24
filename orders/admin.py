4e407be9810a8690724909ca752b924a10c80dcd
Dement0 <ilkeer.ozgur@gmail.com>
2019-02-24 19:57:35 +0100

    Add Size and Toppings to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Pizza
+from .models import Pizza, Size, Topping
 
 
 class PizzaAdmin(admin.ModelAdmin):
@@ -15,8 +15,26 @@ class PizzaAdmin(admin.ModelAdmin):
     # Use list filter to add a sidebar to filter
     list_filter = ['pizza_type']
 
-    # Add a search box to search question texts, uses LIKE query
+    # Add a search box to search pizza names and descriptions, uses LIKE query
     search_fields = ['pizza_name', 'pizza_description']
 
 
+class ToppingAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Topping information', {'fields': ['topping_text']}),
+        ('Topping price', {'fields': ['topping_price']})
+    ]
+
+    # Use list_display, a tuple of field names to display as columns
+    list_display = ('topping_text', 'topping_price')
+
+    # Use list filter to add a sidebar to filter
+    list_filter = ['topping_price']
+
+    # Add a search box to search topping texts
+    search_fields = ['topping_text']
+
+
 admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Size)
+admin.site.register(Topping, ToppingAdmin)
